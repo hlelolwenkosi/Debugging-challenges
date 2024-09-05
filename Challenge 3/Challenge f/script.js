898db1ee44c716b1ecf89c98d010b014baad615a
hlelolwenkosi <1071hf@gmail.com>
2024-09-05 16:15:44 +0200

    fixed bugs

--- Challenge 3/Challenge f/script.js
+++ Challenge 3/Challenge f/script.js
@@ -1,35 +1,28 @@
-const date = 2050
-const status = 'student'
-let count = 0
+const year = 2050;
+const status = 'student';
+let count = 0;
 
-if (date = 2050) {
-	console.log("January", 'New Year’s Day')
-	console.log("March", 'Human Rights Day')
-	date = 'April'
-	console.log(date, 'Family Day')
-	console.log(date, 'Freedom Day')
-	count = count + 4
+if (year === 2050) {
+  console.log("January", 'New Year’s Day');
+  console.log("March", 'Human Rights Day');
+  console.log('April', 'Family Day');
+  console.log('April', 'Freedom Day');
+  count += 4;
 
-	if (status ==="student") {
-	  console.log('June', 'Youth Day')
-	count = count + 1
+  if (status === "student") {
+    console.log('June', 'Youth Day');
+    count += 1;
   }
 
-	console.log('August', 'Women’s Day')
-	console.log('September', 'Heritage Day')
-	date = 'December'
-	console.log(date, 'Day of Reconciliation')
-	 count = count + 3
+  console.log('August', 'Women’s Day');
+  console.log('September', 'Heritage Day');
+  console.log('December', 'Day of Reconciliation');
+  count += 3;
 
-	if (status === "parent") {
-	  console.log(date, 'Christmas Day')
-	count = count + 1
-  }
-
-	console.log(date, 'Day of Goodwill')
-	count = count + 1
+  console.log('December', 'Day of Goodwill');
+  count += 1;
 }
 
-console.log('Your status is:', status)
-console.log('The year is:', date)
-console.log('The total holidays is:', count)
+console.log('Your status is:', status);
+console.log('The year is:', year);
+console.log('The total holidays is:', count);
