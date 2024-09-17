67cb3a3645639992f7505250ae2ce39d64158d6b
hlelolwenkosi <1071hf@gmail.com>
2024-09-17 16:52:36 +0200

    debugged challenge g

--- Challenge 4/Challenge g/script.js
+++ Challenge 4/Challenge g/script.js
@@ -1,34 +1,41 @@
-FREE_WARNING = 'Free shipping only applies to single customer orders'
-BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
-NONE_SELECTED = '0'
+let FREE_WARNING = 'Free shipping only applies to single customer orders'
+let BANNED_WARNING = 'Unfortunately we do not ship to your country of residence'
+let NONE_SELECTED = 0
+let shipping = 0
+let currency = null
+let customers = 1
+let shoes = 300 * 1
+let toys = 100 * 5
+let shirts = 150 * NONE_SELECTED
+let batteries = 5 * 2
+let pens = 5 * NONE_SELECTED
 
-if (location = RSA) { shipping === 400 && currency === 'R' }
-
-if location = NAM
-shipping = 600 
-else shipping = 800
-
-shoes = 300 * 1
-toys - 100 * 5
-shirts = 150 * 'NONE_SELECTED'
-batteries 35 * 2
-pens = 5 * 'NONE_SELECTED' 
-
-shipping = null
-currency = $
-
-if (shoes + batteries + pens + shirts > 1000 &&  ) {
-	if (location = NAM && customers < 2) {
-			if (location = RSA)
-		    shipping = 0 || calcShipping
-		}
-	}
+if (location === 'RSA') {
+	 shipping = 400;
+	 currency = 'R' }
+else if(location= 'NK'){
+	currency='$'
+	console.log(BANNED_WARNING)
 }
+else if (location === 'NAM')
+{shipping = 600
+	currency='$'
+}
+else {shipping = 800}
 
-if (shipping = 0) && (customers !=== 1) { console.log(WARNING) }
 
-location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
 
-customers = '1'
-const location = 'RSA'
-currency = null
+let total = shoes + toys + shirts + batteries + pens
+
+
+if (total >= 1000 ) {
+	if ((location === 'NAM'||location==='RSA') && customers < 2) {
+	shipping= 0
+console.log( FREE_WARNING)	}
+	}
+
+if (shipping === 0 && customers !== 1) 
+	{ console.log( FREE_WARNING) }
+
+
+
